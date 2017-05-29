e02f3d88d512b68c901137a76425288c58a823bf
awesth <apwesth@gmail.com>
2017-05-29 22:24:49 +0200

    .

--- httpClient.py
+++ httpClient.py
@@ -5,31 +5,31 @@ import os
 
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
- 
-  # GET
-  def do_GET(self):
-        # Send response status code
+
+  # GET method.
+  def doGET(self):
+        # Send response status code.
         self.send_response(200)
- 
-        # Send headers
+
+        # Send headers.
         self.send_header('Content-type','text/html')
         self.end_headers()
- 
-        # Send message back to client
+
+        # Send message back to client.
         message = "Hello world!"
-        # Write content as utf-8 data
+        # Write content as utf-8 data.
         self.wfile.write(bytes(message, "utf8"))
         return
- 
+
 def run():
   print('starting server...')
- 
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+
+  # Server settings.
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access.
   server_address = ('127.0.0.1', 8081)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
- 
- 
+
+
 run()
